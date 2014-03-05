185178e05587306713c4d92201f06be8f37c58f4
Juan Pablo Tosso <the.dust.team@gmail.com>
2014-03-04 21:13:08 -0300

    fix

--- install-FruityWifiPlus.sh
+++ install-FruityWifiPlus.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-find FruityWifi -type d -exec chmod 755 {} \;
-find FruityWifi -type f -exec chmod 644 {} \;
+find  -type d -exec chmod 755 {} \;
+find  -type f -exec chmod 644 {} \;
 
 mkdir tmp-install
 cd tmp-install
@@ -173,24 +173,24 @@ fi
 
 cmd=`date +"%Y-%m-%d-%k-%M-%S"`
 cd ../
-mv /usr/share/FruityWifi FruityWifi.BAK.$cmd
-cp -a FruityWifi /usr/share/FruityWifi
-ln -s /usr/share/FruityWifi/www /var/www/FruityWifi
-ln -s /usr/share/FruityWifi/logs /var/www/FruityWifi/logs
+mv /usr/share/ .BAK.$cmd
+cp -a FruityWifiPlus /usr/share/
+ln -s /usr/share/FruityWifiPlus/www /var/www/FruityWifiPlus
+ln -s /usr/share/FruityWifiPlus/logs /var/www/FruityWifiPlus/logs
 mkdir /var/www/tmp
 chown www-data.www-data /var/www/tmp
 chmod  777 /var/www/tmp
-chmod 755 /usr/share/FruityWifi/squid.inject/poison.pl
-ln -s /usr/share/FruityWifi/www.site /var/www/site
-chown -R www-data.www-data /usr/share/FruityWifi/www.site
-chmod 777 /usr/share/FruityWifi/www.site/data.txt
-chmod 777 /usr/share/FruityWifi/www.site/inject/data.txt
+chmod 755 /usr/share/FruityWifiPlus/squid.inject/poison.pl
+ln -s /usr/share/FruityWifiPlus/www.site /var/www/site
+chown -R www-data.www-data /usr/share/FruityWifiPlus/www.site
+chmod 777 /usr/share/FruityWifiPlus/www.site/data.txt
+chmod 777 /usr/share/FruityWifiPlus/www.site/inject/data.txt
 
-#mkdir /FruityWifi/logs/kismet
-#mkdir /FruityWifi/logs/sslstrip
+#mkdir /FruityWifiPlus/logs/kismet
+#mkdir /FruityWifiPlus/logs/sslstrip
 
 # BIN
-cd /usr/share/FruityWifi/bin/
+cd /usr/share/FruityWifiPlus/bin/
 gcc danger.c -o danger
 chmod 4755 danger
 
